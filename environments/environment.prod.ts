60dc059c9f5fa50421f2182c21bf8f00785e7e9c
Pooja <poojaokmoku1999@gmail.com>
2024-03-11 13:28:08 +0530

    environment variable

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  AUTHORIZATION_URL: 'https://localhost:5001/api/User/authenticate',
+  CHECKLIST_URL: "https://localhost:44310/api/AuditChecklist/AuditCheckListQuestions",
+  SEVERITY_URL: "https://localhost:44303/api/AuditSeverity"
 };
